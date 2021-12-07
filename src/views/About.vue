0e468506804b1f09a6230b0f9fb5b9bcd9a19c71
mirasrobert <mrmirasrobert@gmail.com>
2021-12-07 22:04:39 +0800

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col">
+        <span class="fw-bold">Github Finder.</span> Find or search GitHub
+        profile with latest repositories, location, followers and etc. Created
+        by
+        <span>
+          <a href="https://github.com/mirasrobert">Robert Miras</a>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
